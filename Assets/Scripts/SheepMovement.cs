1443b747f4ccd97804c7a231818214521f587585
jhsi05001 <125725313+jhsi05001@users.noreply.github.com>
2026-06-26 15:10:08 -0700

    added sheep idle and flee mechanics

--- Assets/Scripts/SheepMovement.cs
+++ Assets/Scripts/SheepMovement.cs
@@ -6,24 +6,22 @@ public class SheepMovement : MonoBehaviour
     public Transform player;
 
     [Header("Run Settings")]
-    public float runDistance = 20f;
-    public float panicDistance = 5f;
+    public float runDistance = 20f; // distance sheep will run away
+    public float fleeRange = 5f; // range at which sheep will start running away
+    public float sheepRunSpeed = 6f; // speed at which sheep runs away
 
     [Header("Idle Settings")]
-    public float idleDistance = 4f;
-    public float idleTime = 5f;
+    public float idleDistance = 2f; // distante at which sheep idles from spot
+    public float idleTime = 2f;
+    public float sheepIdleSpeed = 3f; // speed at which sheep moves when idle
 
     private NavMeshAgent navMeshAgent;
-    private float idleTimer;
+    private float idleTimer = 0;
 
     private void Awake()
     {
         navMeshAgent = GetComponent<NavMeshAgent>();
-    }
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
+        idleTimer = idleTime;
     }
 
     // Update is called once per frame
@@ -34,8 +32,8 @@ public class SheepMovement : MonoBehaviour
         // find current distance to player
         float currDistance = Vector3.Distance(transform.position, player.position);
 
-        // if within distance, run
-        if (currDistance < panicDistance)
+        // if within FLEE distance, run
+        if (currDistance < fleeRange)
         {
             // find the direction in lieu to player
             Vector3 directionToSheep = transform.position - player.position;
@@ -43,13 +41,40 @@ public class SheepMovement : MonoBehaviour
             Vector3 newDestination = transform.position + directionToSheep.normalized * runDistance;
             // set destination
             navMeshAgent.SetDestination(newDestination);
-
-            // set a timer
-            idleTimer = idleTime;
+            navMeshAgent.speed = sheepRunSpeed;
         }
+        // if within PANIC distance, run faster 
+
+        // if safe, IDLE
         else // want sheep to idle
         {
-            
+            // set speed
+            navMeshAgent.speed = sheepIdleSpeed;
+
+            idleTimer += Time.deltaTime;
+
+            if (idleTimer >= 5f && !navMeshAgent.hasPath && navMeshAgent.remainingDistance <= navMeshAgent.stoppingDistance)
+            {
+                idleTimer = 0; // reset idle timer
+
+                // set IDLE destination
+                Vector3 randDirection = Random.insideUnitSphere * idleDistance;
+                Vector3 newDestination = transform.position + randDirection;
+                
+                // check if newDestination is on the NavMesh
+                NavMeshHit hit;
+                if (NavMesh.SamplePosition(newDestination, out hit, idleDistance, NavMesh.AllAreas))
+                {
+                    newDestination = hit.position;
+                }
+                else
+                {
+                    newDestination = transform.position;
+                }
+
+                // set destination
+                navMeshAgent.SetDestination(newDestination);
+            }
         }
         
     }
